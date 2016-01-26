9bec3d481c9d808b30d79173ffe250767a6e9bc4
Smallpath <smallpath2013@gmail.com>
2016-01-26 23:30:17 +0800

    add functions to control files and elements

--- Sp_memory/lib/SubSystems.jsx
+++ Sp_memory/lib/SubSystems.jsx
@@ -1,4 +1,72 @@
-﻿var presetWindow = function () {
+﻿var upAndDown=function (isUp,isW){
+                var file = sp.getFileByName(sp.droplist.selection.text);
+                var xml = new XML(file.readd());
+        if (isUp == true &&sp.gv.lastSelectedItem != null &&sp.gv.lastSelectedItem.index > 0) {
+                var ai = sp.gv.lastSelectedItem.index;
+                var upxml = new XML(xml.child(sp.gv.lastSelectedItem.index));
+                xml.insertChildBefore(xml.child(sp.gv.lastSelectedItem.index-1), upxml);
+                xml.child(sp.gv.lastSelectedItem.index + 1).setLocalName("waitToDelete");
+                delete xml.waitToDelete;
+                file.writee(xml);
+                sp.gv.lastSelectedItem.moveUp();
+                var thisItem = sp.gv.lastSelectedItem;
+            } else if (isUp == false && sp.gv.lastSelectedItem != null && sp.gv.lastSelectedItem.index < xml.children().length() - 1) {
+                var ai = sp.gv.lastSelectedItem.index;
+                var downxml = new XML(xml.child(sp.gv.lastSelectedItem.index));
+                xml.insertChildAfter(xml.child(sp.gv.lastSelectedItem.index+1), downxml);
+                xml.child(sp.gv.lastSelectedItem.index).setLocalName("waitToDelete");
+                delete xml.waitToDelete;
+                file.writee(xml);
+                sp.gv.lastSelectedItem.moveDown();
+                var thisItem = sp.gv.lastSelectedItem;
+            }
+        }
+
+var upAndDownWindow=function (cu){
+        var udWin = new Window("palette",loc(sp.ud));
+        udWins=udWin.add("Group{}");
+        var a = udWins.add("Button{text:'"+loc(sp.up)+"'}");
+        var b = udWins.add("Button{text:'"+loc(sp.down)+"'}");
+        var c = udWins.add("Group{et:EditText{text:'0',characters:3,justify:'center'},j:Button{text:'"+loc(sp.jmp)+"'}}");
+        udWin.frameLocation = cu;
+        udWin.show();
+        a.onClick = function(){
+                upAndDown(true, true);
+            }
+        b.onClick = function(){
+                upAndDown(false, true);
+            }
+        c.j.onClick = function(){
+                var d =parseInt(c.et.text);
+                var file = sp.getFileByName(sp.droplist.selection.text);
+                var xml = new XML(file.readd());
+                if(sp.gv.children.length ==0 ) return;
+                if(sp.gv.lastSelectedItem ==null) return;
+                if(d>=0 && d< sp.gv.children.length-1 && sp.gv.lastSelectedItem.index != d){
+                        var upxml = new XML(xml.child(sp.gv.lastSelectedItem.index));
+                        xml.insertChildBefore(xml.child(d), upxml);
+                        xml.child(sp.gv.lastSelectedItem.index + 1).setLocalName("waitToDelete");
+                        delete xml.waitToDelete;
+                        file.writee(xml);
+                        sp.gv.lastSelectedItem.moveBefore(sp.gv.children[d]);
+                    }else if(d == sp.gv.children.length-1 && sp.gv.lastSelectedItem.index != d){
+                        var upxml = new XML(xml.child(sp.gv.lastSelectedItem.index));
+                        xml.insertChildAfter(xml.child(d), upxml);
+                        xml.child(sp.gv.lastSelectedItem.index + 1).setLocalName("waitToDelete");
+                        delete xml.waitToDelete;
+                        file.writee(xml);
+                        sp.gv.lastSelectedItem.moveAfter(sp.gv.children[d]);
+                    }else{
+                        try{
+                            alert(loc(sp.from)+"~"+(sp.gv.children.length-1).toString());
+                            }catch(er){}
+                        }
+            }
+        }
+
+
+
+var presetWindow = function () {
                     var jinWin = new Window("dialog", loc(sp.settingPre));
                     jinRes = "group{\
                                         orientation:'column',alignment:['fill','fill'],alignChildren:['fill','fill'],\
--- memory.jsx
+++ memory.jsx
@@ -4,6 +4,7 @@
     #include 'Sp_memory/lib/UIParser.jsx'
     #include 'Sp_memory/lib/HelperScripts.jsx'
     #include 'Sp_memory/lib/SubSystems.jsx'
+    #include 'Sp_memory/lib/RightClickMenu.jsx'
     var fns = sp.fns = new fns();
     
     var win = global instanceof Panel ? global : new Window("window",sp.scriptName, undefined, {resizeable: true});
@@ -27,8 +28,9 @@
                          }else if (key.ctrlKey == false){
                                 fns.newItem(event);
                              }else if (key.ctrlKey == true){
-//~                                  currentPosition=[event.screenX-152,event.screenY];
-//~                                  newFunc.ud2(currentPosition);
+                                 currentPosition=[event.screenX-152,event.screenY];
+                                 upAndDownWindow(currentPosition)
+
                                  }
     }
 
@@ -61,18 +63,48 @@
             
             this.newLayer  = function(){
 
-                  }
-          
-            this.deleteItem  = function(){
-                  
                   },
-            
+
             this.newItem  = function(){
 
                   },
-          
+
+            this.deleteItem  = function(){
+                          if(sp.gv.selection.length == 0) return;
+                          if(sp.deleteAlertValue == true)
+                              var sure = confirm(loc(sp.sureDelete));
+                          if(sp.deleteAlertValue == true && sure== false)  return;
+
+                          var file = sp.getFileByName(sp.droplist.selection.text);
+                          var xml = new XML(file.readd());
+                          sp.gv.selection.forEach(function(item,index){
+                                      xml.child(item.index).setLocalName("waitToDelete");
+                                      var preText = item.text;
+                                      var image = sp.getImageFile(sp.droplist.selection.text,preText);
+                                      if(image.exists)
+                                          image.remove();    
+                                });
+                          delete xml.waitToDelete;
+                          if(xml.children().length() !=0){
+                                  file.writee(xml);
+                             }else{
+                                  file.writee("<tree></tree>");
+                             }
+                        
+                          sp.gv.removeAll();
+                          sp.droplist.notify("onChange");
+                          sp.gv.refresh();
+                  },
+
             this.importImage  = function(){
-                  
+                        if(!sp.gv.lastSelectedItem) return;
+                        var file = File.openDialog("Please select pictures", false);
+                        if(!file) return;
+                        if(file.name.split(".").last() != "jpg" && file.name.split(".").last() != "png") return;
+                        var imageFile = sp.getImageFile(sp.droplist.selection.text,sp.gv.lastSelectedItem.text);
+                        sp.cropImage(file,imageFile);
+                        sp.gv.lastSelectedItem.image = imageFile;
+                        sp.gv.refresh();
                   },
           
             this.deleteGroup  = function(){
@@ -84,7 +116,7 @@
                         var imageFolder = sp.getImageFolderByName(sp.droplist.selection.text);
                         var images = imageFolder.getFiles();
                         images.forEach(function(item){
-                              item.remove();
+                                    item.remove();
                               });
                         imageFolder.remove();
                         
@@ -100,11 +132,56 @@
                   },
           
             this.addGroup  = function(){
-                  
+                    var newEleName = prompt(loc(sp.setName), "Default");
+                    if (!newEleName){ alert(loc(sp.blankName));return;}
+                    if(sp.lookUpTextInChildren(newEleName,sp.droplist.items)){alert(loc(sp.existName));return;}
+                    var file = sp.getFileByName(newEleName);
+                    sp.getImageFolderByName(newEleName);
+                    var str = "<tree></tree>";
+                    file.writee(str);
+                    var xml = new XML(sp.settingsFile.readd());
+                    xml.ListItems.appendChild(new XML("<Name>" + newEleName + "</Name>"));
+                    sp.settingsFile.writee(xml);
+                    sp.reloadDroplist();
+                    sp.droplist.selection = sp.droplist.items.length -1;
+                    sp.gv.refresh();
                   },
           
             this.exportFile  = function(){
-                  
+                        var exportFolder = Folder.selectDialog("Please select folder");
+                        if(!exportFolder) return;
+                        if(!(exportFolder instanceof Folder)) return;
+                        var sourceFile = sp.getFileByName(sp.droplist.selection.text);
+                        var targetFile = File(exportFolder.toString() + sp.slash + sp.droplist.selection.text + ".xml");
+                        if(targetFile.exists) {alert(loc(sp.overWritten)); return; }
+                        
+                        var images= sp.getImageFolderByName(sp.droplist.selection.text).getFiles();
+                        var picXml = new XML("<pic></pic>");
+                        images.forEach(function(item,index){
+                                      if(item.name.indexOf(".png") !=1){
+                                                  item.open("r");
+                                                  item.encoding = "binary";
+                                                  var str = encodeURIComponent (item.read());
+                                                  item.close();
+                                                  var tempXmlBigHere=new XML("<imgName>"+encodeURIComponent(item.name)+"</imgName>");
+                                                  var tempXmlHeres=new XML("<img>"+str+"</img>");
+                                                  var guluTempA=new XML("<imgInfo></imgInfo>");
+                                                  guluTempA.appendChild(tempXmlBigHere);
+                                                  guluTempA.appendChild(tempXmlHeres);
+                                                  picXml.appendChild (guluTempA);
+                                            }
+                              });
+                        var xml = new XML(sourceFile.readd());
+                        if(picXml.children().length()>0){
+                                    xml.appendChild (picXml);
+                              }
+                        if(xml.children().length()==0){
+                                    xml = "<tree></tree>"
+                              }
+                        targetFile.writee(xml);
+                        clearOutput();
+                        writeLn("Complete!");
+                              
                   },
           
           
@@ -133,7 +210,7 @@
                                sp.settingsFile.writee(xml.toString());
                         });
                     sp.reloadDroplist();
-                    sp.droplist.selection = selectionIndex;
+                    sp.droplist.selection = sp.droplist.items.length -1 ;
                     sp.gv.refresh();
                   },
             //~   the function which change element name
@@ -228,220 +305,7 @@
                 }
         }
 
-    function createMenu(CurrentPosition) {
-        try{
-        var StringList = [loc(sp.settings), loc(sp.deleteGroup), loc(sp.importFile), loc(sp.exportFile), loc(sp.addGroup),loc(sp.importPicture), loc(sp.addElement), loc(sp.deleteElement), loc(sp.create), loc(sp.cover), loc(sp.isShow), loc(sp.isAlert), loc(sp.isPrecomp), loc(sp.isOffset), loc(sp.isName), loc(sp.isEffect),loc(sp.cleanProperty),loc(sp.offsetKey)];
-        var length = StringList.length;
-        var Space = 102 / 5;
-        var h;
-        var buttonWidth = 40;
-        var checkBoxWidth = 41;
-        var buttonHeight = 20;
-        var checkBoxHeight = 21;
-        if (sp.lang == "ch") 
-            var h = 180;
-        else
-            var h = 190;
-        var ShortMenu = new Window("palette", "huhu", [0, 0, h, length * Space / 2 + 42], {borderless: true});
 
-            for (var n = 0; n < length; n++) {
-                if (n == 0) {
-                    ShortMenu._0=ShortMenu.add("button",[0,0,h/2,22],StringList[0]);
-                } else if (n <= 9) {
-                    if (n % 2 == 0) {
-                        ShortMenu["_" + (n + 1) ] =ShortMenu.add("button",[0,(parseInt((n) / 2) * buttonHeight + buttonWidth),h/2,(22 + parseInt((n) / 2) * buttonHeight + buttonWidth)],StringList[n]);
-                    } else {
-                        ShortMenu["_" + (n + 1) ] =ShortMenu.add("button",[h/2,(parseInt((n-1) / 2) *buttonHeight +buttonWidth),h,(22 + parseInt((n-1) / 2) * buttonHeight +buttonWidth)],StringList[n]);
-                    }
-                } else {
-                    if (n % 2 == 0) {
-                        ShortMenu["_" + (n + 1) ] =ShortMenu.add("checkbox",[0,(parseInt((n) / 2) * checkBoxHeight +checkBoxWidth),h/2,(22 + parseInt((n) / 2) * checkBoxHeight + checkBoxWidth)],StringList[n]);
-                   } else {
-                        ShortMenu["_" + (n + 1) ] =ShortMenu.add("checkbox",[h/2,(parseInt((n-1) / 2) * checkBoxHeight + checkBoxWidth),h,(22 + parseInt((n-1) / 2) * checkBoxHeight + checkBoxWidth)],StringList[n]);
-                   }
-                }
-            }
-            ShortMenu._yushe=ShortMenu.add("button",[0,20,h,42],loc(sp.yushe));
-            ShortMenu._c = ShortMenu.add("button", [0, 42, h/2, 62], loc(sp.changeName));
-            ShortMenu._1 = ShortMenu.add("dropdownlist", [h/2, 0,h, 22]);
-            ShortMenu._1.add("item", loc(sp.save));
-            ShortMenu._1.add("item", loc(sp.exp));
-            ShortMenu._1.add("item", loc(sp.aep));
-            ShortMenu._1.add("item", loc(sp.preset));
-            ShortMenu._1.add("item", loc(sp.curve));
-            ShortMenu._1.selection = 0;
-            ShortMenu._1.enabled = true;
-            ShortMenu._11.value = sp.showThumbValue ;
-            ShortMenu._12.value = sp.deleteAlertValue ;
-            ShortMenu._13.value = sp.preComposeValue;
-            ShortMenu._14.value = sp.saveMaterialValue;
-            ShortMenu._15.value = sp.autoNameValue;
-            ShortMenu._16.value = sp.onlyEffectValue;
-            ShortMenu._17.value = sp.cleanGroupValue;
-            ShortMenu._18.value = sp.offsetKeyframeValue;
-
-            ShortMenu._1.onChange=ShortMenu._1.onChanging =function(){
-                      try {     
-                        //run sp_translate script
-                        this.selection.index == 1 &&
-                            translate()  ||
-                        
-                        //generate and then save the whole group
-                        this.selection.index == 2 &&
-                           reloadPic()  ||
-                        
-                        //auto save every layer in current comp,one layer as one element
-                        this.selection.index == 3 &&
-                           autoSave()  ||
-                        
-                        //cut layers' length by opacity and comp length
-                        this.selection.index == 4 &&
-                           cutLength();
-                           
-                        } catch (err) {err.printa();}
-                        
-                        //back list's selection
-                        this.selection = 0;
-                }
-          
-            var isCheckBoxClicked = false;
-
-            
-            
-            ShortMenu._yushe.onClick=function(){
-                presetWindow();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-                }
-            
-            ShortMenu._c.onClick = function(){
-                    sp.fns.changeName();
-                    isCheckBoxClicked = false;
-                    ShortMenu.close();
-                }
-            
-            ShortMenu._0.onClick = function() {
-                settingsButtonFunc();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-            }
-            
-            ShortMenu._2.onClick = function() {
-                sp.fns.deleteGroup();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-            }
-      
-            ShortMenu._3.onClick = function() {
-                sp.fns.importFiles();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-            }
-      
-            ShortMenu._4.onClick = function() {
-                sp.fns.exportFile ();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-            }
-      
-            ShortMenu._5.onClick = function() {
-                sp.fns.addGroup();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-            }
-      
-            ShortMenu._6.onClick = function() {
-                sp.fns.importImage();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-            }
-      
-            ShortMenu._7.onClick = function() {
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-                sp.fns.newItem();
-            }
-      
-            ShortMenu._8.onClick = function() {
-                sp.fns.deleteItem();
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-            }
-      
-            ShortMenu._9.onClick = function() {
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-                sp.fns.newLayer();
-            }
-      
-            ShortMenu._10.onClick = function() {
-                isCheckBoxClicked = false;
-                ShortMenu.close();
-                sp.fns.cover();
-            }
-      
-            ShortMenu._11.onClick = function(){
-                     sp.showThumbValue = this.value;
-                     gv.showText = this.value;
-                     sp.saveSetting("showThumb",this.value.toString());
-                     isCheckBoxClicked = true;
-                     sp.gv.refresh();
-                }            
-            ShortMenu._12.onClick = function(){
-                     sp.deleteAlertValue = this.value;
-                     sp.saveSetting("deleteAlert",this.value.toString());
-                     isCheckBoxClicked = true;
-                }      
-            ShortMenu._13.onClick = function(){
-                     sp.preComposeValue = this.value;
-                     sp.saveSetting("preCompose",this.value.toString());
-                     isCheckBoxClicked = true;
-                }      
-            ShortMenu._14.onClick = function(){
-                     sp.saveMaterialValue = this.value;
-                     sp.saveSetting("saveMaterial",this.value.toString());
-                     isCheckBoxClicked = true;
-                }      
-            ShortMenu._15.onClick = function(){
-                     sp.autoNameValue = this.value;
-                     sp.saveSetting("autoName",this.value.toString());
-                     isCheckBoxClicked = true;
-                }      
-            ShortMenu._16.onClick = function(){
-                     sp.onlyEffectValue = this.value;
-                     sp.saveSetting("onlyEffect",this.value.toString());
-                     isCheckBoxClicked = true;
-                }      
-            ShortMenu._17.onClick = function(){
-                     sp.cleanGroupValue = this.value;
-                     sp.saveSetting("cleanGroup",this.value.toString());
-                     isCheckBoxClicked = true;
-                }      
-            ShortMenu._18.onClick = function(){
-                     sp.offsetKeyframeValue = this.value;
-                     sp.saveSetting("offsetKeyframe",this.value.toString());
-                     isCheckBoxClicked = true;
-                }
-
-        
-    
-        ShortMenu.addEventListener("blur", function() {
-            if (isCheckBoxClicked == false) {
-                ShortMenu.close();
-            } else {
-                isCheckBoxClicked = true;
-            }
-        });
-    
-        ShortMenu.onDeactivate = function() {
-            ShortMenu.close();
-        }
-    
-        ShortMenu.frameLocation = CurrentPosition;
-        ShortMenu.show();
-        ShortMenu.addEventListener("keydown",function(event){ShortMenu.close();});
-        }catch(err){err.printa()}
-      }
 
 
     }//~ fns function end
