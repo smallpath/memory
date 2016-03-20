0efe9c05dbecc7c6989fea6d4cea64d0ae90f3e7
Smallpath <smallpath2013@gmail.com>
2016-03-20 12:10:36 +0800

    release Sp_toolbar and Sp_translate

--- Sp_translate.jsx
+++ Sp_translate.jsx
@@ -1,7 +1,13 @@
-﻿/*************************************Sp_translate v1.7,自动修复多语言版本造成的表达式报错问题**************************************/
-function translate(thisObj,expProps) {
+﻿translate(this);
+
+
+  function translate(thisObj,expProps) {
   var you = this;
-  
+
+ function loc(obj){
+        return obj["ch"];
+     }
+ 
 var tsp={
     trans:{en:'Translate',ch:'开始'},
     allComp:{en:'Translate all comps',ch:'翻译所有合成'},
@@ -386,7 +392,7 @@ var tsp={
     var checkFile=groupb.checkFile;
     var addbtn=winTempA.addbtn;
     var outFile;
-
+    
     lista.selection = 2;
 
   about.onClick = function() {
@@ -752,164 +758,3 @@ if(typeof expProps =="undefined"){
         }
   }
 }
-
-/*************************************Sp_cutLength v1.1,自动裁剪图层长度,根据普通层的透明度与合成层内容的长度进行裁剪**************************************/
-function cutLength(){
-                if (confirm (loc(sp.cutLength)) == false) return;
-                var thisComp = app.project.activeItem;
-
-Array.prototype.search = function(isZero){
-    if (isZero==0){
-        isOk = false;
-        for (b=0;b<this.length-1;b++){
-            if (this[b]==0&&this[b+1]>0){
-            isOk = true;
-            break;
-            }
-            }
-        if (isOk ==true){
-            return b;
-            }else{
-                return -1;
-                }
-        }else if(isZero == 100){
-        isOk = false;
-        for (b=this.length-1;b>0;b--){
-            if (this[b-1]>0&&this[b]==0){
-            isOk = true;
-            break;
-            }
-            }
-        if (isOk ==true){
-            return b;
-            }else{
-                return -1;
-                }
-            }
-    }
-app.beginUndoGroup ("Undo crop");
-try{cutLayers(thisComp,new Object);}catch(err){}
-app.endUndoGroup();
-clearOutput();
-writeLn("Complete!");
-
-function cutLayers(comp,obj){
-    try{
-    if (obj.hasOwnProperty("_"+comp.id)){
-        }else{
-            eval("obj._"+comp.id+"={};");
-            eval("obj._"+comp.id+".inPointArr=[];");
-            eval("obj._"+comp.id+".outPointArr=[];");
-            }
-        }catch(err){}
-    
-    for (var i=0;i<comp.layers.length;i++){
-         try{ if(comp.layer(i+1).source instanceof CompItem ){
-                 if (obj.hasOwnProperty("_"+comp.layer(i+1).source.id)){ 
-                     }else{
-              cutLayers(comp.layer(i+1).source,obj);
-              }
-                try{eval("obj._"+comp.layer(i+1).source.id+".inPointArr.sort(function (a,b){return a-b;})");}catch(err){}
-                try{eval("obj._"+comp.layer(i+1).source.id+".outPointArr.sort(function (a,b){return b-a;})");}catch(err){}
-                if (eval("comp.layer(i+1).inPoint-comp.layer(i+1).startTime<obj._"+comp.layer(i+1).source.id+".inPointArr[0]")){
-                try{eval("comp.layer(i+1).inPoint=comp.layer(i+1).startTime+obj._"+comp.layer(i+1).source.id+".inPointArr[0]");}catch(err){}
-                }
-                if (eval("comp.layer(i+1).outPoint-comp.layer(i+1).startTime>obj._"+comp.layer(i+1).source.id+".outPointArr[0]")){
-                try{eval("comp.layer(i+1).outPoint=comp.layer(i+1).startTime+obj._"+comp.layer(i+1).source.id+".outPointArr[0]");}catch(err){}
-                }
-              }
-            try{eval("obj._"+comp.id+".inPointArr.push(comp.layer(i+1).inPoint);");}catch(err){}
-            try{eval("obj._"+comp.id+".outPointArr.push(comp.layer(i+1).outPoint);");}catch(err){}
-            cut(comp.layer(i+1));
-            }catch(err){}
-        } 
-    }
-
-function cut(layer){
-    thisOpa = layer.transform.opacity;
-    thisKeysNum = thisOpa.numKeys;
-    var arr=[];
-    for (a=0;a<thisKeysNum;a++){
-        arr.push(thisOpa.keyValue(a+1));
-        }
-    if (arr.length==0){
-        if (thisOpa.value == 0){
-            layer.inPoint = 0;
-            layer.outPoint = 0 + layer.containingComp.frameDuration;
-            }
-        }
-    else if(arr.length==1){
-        if (arr[0] == 0){
-            layer.inPoint = thisOpa.keyTime(1);
-            layer.outPoint = thisOpa.keyTime(1)+layer.containingComp.frameDuration;
-            }
-        }
-    else if (arr.length>1){
-            if (arr.search(0)!=-1){
-                if (layer.inPoint<thisOpa.keyTime(arr.search(0)+1) && arr.search(0)  == 0){
-                    layer.inPoint = thisOpa.keyTime(arr.search(0)+1);
-                }
-                }
-            if (thisOpa.keyValue(thisKeysNum)==0){
-                    layer.outPoint = thisOpa.keyTime(thisKeysNum);
-                }else{
-                    
-                    }
-            }
-        }
-    }
-/*************************************批量自动保存每一层为新Item**************************************/
-function autoSave(){
-                        if(confirm(loc(sp.auto))==false) return;
-                        if(!(app.project.activeItem instanceof CompItem)) return;
-                        try{
-                            var preRenameValue=sp.autoNameValue;
-                            sp.autoNameValue=true;
-                            for(var i=0;i<app.project.activeItem.numLayers;i++){
-                                  for (var j= 1; j <=app.project.activeItem.numLayers; j++){
-                                    app.project.activeItem.layer(j).selected = false;
-                                  }
-                                app.project.activeItem.layer(i+1).selected = true;
-                                sp.fns.newItem();
-                                app.project.activeItem.layer(i+1).selected = false;
-                                }
-                            sp.autoNameValue=preRenameValue;
-                            }catch(err){}
-                            sp.droplist.notify("onChange");
-                            sp.gv.refresh();
-}
-
-/*************************************自动重载图片**************************************/
-function reloadPic(){
-                    if(confirm (loc(sp.refresh))==false)  return;
-                    try{
-                            var preRenameValue=sp.autoNameValue;
-                            sp.autoNameValue=true;
-                            var preCompValue=sp.preComposeValue;
-                            sp.preComposeValue=false;
-                        for(var i=0;i<sp.gv.children.length;i++){
-                            try{
-                            sp.gv.children[i].selected = true;
-                            try{
-                              sp.gv.children[i-1].selected = true;
-                            }catch(err){}
-                            sp.gv.lastSelectedItem = sp.gv.children[i];
-                            var layerArr = sp.fns.newLayer();
-                            for(var j=0;j<thisComp.selectedLayers.length;j++){
-                                thisComp.selectedLayers[j].selected = false;
-                                }
-                            for(var j=0;j<layerArr.length;j++){
-                                try{thisComp.layer(layerArr[j]).selected = true;}catch(err){}
-                                }
-                           sp.fns.cover();
-                            for(var j=layerArr.length-1;j>=0;j--){
-                                try{thisComp.layer(layerArr[j]).remove();}catch(err){}
-                                }
-                                sp.gv.children[i].selected = false;
-                                }catch(err){alert(err.line)}
-                            }
-                            sp.autoNameValue=preRenameValue;
-                            sp.preComposeValue=preCompValue;
-                            sp.gv.refresh();
-                        }catch(err){}
-                    }
